b5a1545aa2709160c4b54d68c7387edd19be808b
shuja <shuja.devops@gmail.com>
2024-12-08 15:32:09 +0530

    loops example

---
+++ 05-loops/loops.tf
@@ -0,0 +1,6 @@
+resource "aws_instance" "frontend" {
+  count =  5
+  ami           = data.aws_instance.private_ip.ami
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-08f41a3b66746e56a"]
+}
