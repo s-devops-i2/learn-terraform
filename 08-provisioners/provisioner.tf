0af6dcb5230d448df86af5f7efb7f2d5378ef42b
shuja <shuja.devops@gmail.com>
2024-12-08 21:17:19 +0530

    provisioner example

---
+++ 08-provisioners/provisioner.tf
@@ -0,0 +1,21 @@
+
+resource "aws_instance" "frontend" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-08f41a3b66746e56a"]
+
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = self.public_ip
+    }
+
+    inline = [
+      "sudo dnf install nginx",
+      "sudo systemctl start nginx",
+    ]
+  }
+}
