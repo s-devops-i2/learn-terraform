1d0ff57c2e0e0a076723fd5975aad0963c789142
shuja <shuja.devops@gmail.com>
2025-01-14 20:33:39 +0530

    creating ngw igw and route tables to get internet

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -25,6 +25,11 @@ resource "aws_route_table" "frontend-rt" {
     vpc_peering_connection_id = var.peer_connection_id
 
   }
+  route {
+    cidr_block                = "0.0.0.0"
+    gateway_id                = aws_nat_gateway.ngw[count.index].id
+
+  }
 
   tags = {
     Name = "${var.env}-frontend-rt${count.index+1}"
@@ -54,6 +59,10 @@ resource "aws_route_table" "backend-rt" {
   route {
     cidr_block                = var.default_vpc_cidr_block
     vpc_peering_connection_id = var.peer_connection_id
+  }
+  route {
+    cidr_block                = "0.0.0.0"
+    gateway_id                = aws_nat_gateway.ngw[count.index].id
 
   }
 
@@ -85,6 +94,10 @@ resource "aws_route_table" "db-rt" {
   route {
     cidr_block                = var.default_vpc_cidr_block
     vpc_peering_connection_id = var.peer_connection_id
+  }
+  route {
+    cidr_block                = "0.0.0.0"
+    gateway_id                = aws_nat_gateway.ngw[count.index].id
 
   }
 
@@ -110,6 +123,51 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
+resource "aws_route_table" "public-rt" {
+  count  = length(var.public_subnets)
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block                = var.default_vpc_cidr_block
+    vpc_peering_connection_id = var.peer_connection_id
+  }
+  route {
+    cidr_block                = "0.0.0.0"
+    gateway_id                = aws_internet_gateway.igw.id
+
+  }
+
+  tags = {
+    Name = "${var.env}-public-rt${count.index+1}"
+  }
+}
+resource "aws_route_table_association" "public-rt-assoc" {
+  count          = length(var.public_subnets)
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public-rt[count.index].id
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}
+
+resource "aws_eip" "eip" {
+  count    = length(var.public_subnets)
+  domain   = "vpc"
+}
+
+resource "aws_nat_gateway" "ngw" {
+  count         = length(var.public_subnets)
+  allocation_id = aws_eip.eip[count.index].id
+  subnet_id     = aws_subnet.public_subnet[count.index].id
+
+  tags = {
+    Name = "${var.env}-ngw${count.index+1}"
+  }
+}
 
 resource "aws_vpc_peering_connection" "peering" {
   peer_vpc_id = var.default_vpc_id
